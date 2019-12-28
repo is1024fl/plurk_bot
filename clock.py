881bd61c2b9dc0c4ab1e518b8cb12bf4284ac9b8
Fang Lee <ss10240tw@gmail.com>
2019-12-28 20:59:35 +0800

    clock change

--- clock.py
+++ clock.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     sched = BlockingScheduler()
 
-    @sched.scheduled_job('interval', seconds=10)
+    @sched.scheduled_job('interval', seconds=30)
     def timed_job():
         subprocess.call('python -u replurk.py scheduler', shell=True, close_fds=True)
 
